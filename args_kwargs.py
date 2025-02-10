396dd216c9df13ca323175a965c34cbc5a613514
Rogério Balloussier <rogerioballoussier@icloud.com>
2025-02-10 19:08:52 -0300

    added commit...

---
+++ args_kwargs.py
@@ -0,0 +1,25 @@
+def calculate(*args, **kwargs):
+    soma = 0
+
+    if args:
+        soma = sum(args)
+
+    if kwargs:
+        bonus = kwargs.get("bonus")
+        multiplication = kwargs.get("multiplication")
+
+        if bonus:
+            soma += bonus
+
+        if multiplication:
+            soma *= multiplication
+
+    return soma
+
+
+print(calculate(1, 2, 3, 4, 5, 6, 7, 8, 9, bonus=100, multiplication=3))
+print(calculate(1, 2, 3, 4, 5, 6, 7, 8, 9, bonus=100))
+print(calculate(1, 2, 3, 4, 5, 6, 7, 8, 9, multiplication=3))
+print(calculate(1, 2, 3, 4, 5, 6, 7, 8, 9))
+print(calculate(bonus=100))
+print(calculate(multiplication=3))
