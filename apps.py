09e6808e8fc1570f6135beab966277e861680861
Rogério Balloussier <rogerioballoussier@icloud.com>
2025-04-27 22:59:36 -0300

    added commit...

---
+++ apps.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Meus Apps",
+    layout="wide",
+)
+
+st.markdown(
+    body="""<style>
+        [data-testid='stHeader'] {display: none;}
+        #MainMenu {visibility: hidden} footer {visibility: hidden}
+    </style>""",
+    unsafe_allow_html=True
+)
+
+st.navigation(
+    pages={
+        "Home": [
+            st.Page(page="contracheque.py", title="Meu Contracheque BB", icon=":material/payments:"),
+            st.Page(page="megasena.py", title="Sorteio de Mega-Sena", icon=":material/price_check:"),
+            st.Page(page="unibb.py", title="Meus Cursos UniBB", icon=":material/book_5:"),
+        ],
+    },
+    expanded=True
+).run()
