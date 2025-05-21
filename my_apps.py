212578ff10afc50152ad4da4f704ef0a0a5b55a7
Rogério Balloussier <rogerioballoussier@icloud.com>
2025-05-21 19:20:02 -0300

    updated commit...

--- my_apps.py
+++ my_apps.py
@@ -11,7 +11,7 @@ st.set_page_config(
 with open("styles/styles.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-st.header(":material/logo_dev: Meus Apps")
+st.markdown("#### :material/logo_dev: Meus Apps")
 
 col1, col2, col3 = st.columns(3, border=True)
 
@@ -25,20 +25,20 @@ with col1.expander("Cotação de Moedas Estrangeiras", icon="💰"):
         get_cota_eu = requests.get("https://economia.awesomeapi.com.br/last/EUR-BRL").json()
         get_cota_gb = requests.get("https://economia.awesomeapi.com.br/last/GBP-BRL").json()
 
-        st.caption("**:material/attach_money: Cotação do Dólar**")
-        st.caption(f"Moeda $   : {get_cota_us['USDBRL']['name']}")
-        st.caption(f"Data/Hora : {format_date_br(get_cota_us['USDBRL']['create_date'])}")
-        st.caption(f"Valor (R$): {get_cota_us['USDBRL']['bid'].replace('.', ',')}")
-        st.caption("")
-        st.caption("**:material/euro: Cotação do Euro**")
-        st.caption(f"Moeda $   : {get_cota_eu['EURBRL']['name']}")
-        st.caption(f"Data/Hora : {format_date_br(get_cota_eu['EURBRL']['create_date'])}")
-        st.caption(f"Valor (R$): {get_cota_eu['EURBRL']['bid'].replace('.', ',')}")
-        st.caption("")
-        st.caption("**:material/currency_pound: Cotação do Libra**")
-        st.caption(f"Moeda $   : {get_cota_gb['GBPBRL']['name']}")
-        st.caption(f"Data/Hora : {format_date_br(get_cota_gb['GBPBRL']['create_date'])}")
-        st.caption(f"Valor (R$): {get_cota_gb['GBPBRL']['bid'].replace('.', ',')}")
+        st.markdown("**:material/attach_money: Cotação do Dólar**")
+        st.markdown(f"Moeda $   : {get_cota_us['USDBRL']['name']}")
+        st.markdown(f"Data/Hora : {format_date_br(get_cota_us['USDBRL']['create_date'])}")
+        st.markdown(f"Valor (R$): {get_cota_us['USDBRL']['bid'].replace('.', ',')}")
+        st.markdown("")
+        st.markdown("**:material/euro: Cotação do Euro**")
+        st.markdown(f"Moeda $   : {get_cota_eu['EURBRL']['name']}")
+        st.markdown(f"Data/Hora : {format_date_br(get_cota_eu['EURBRL']['create_date'])}")
+        st.markdown(f"Valor (R$): {get_cota_eu['EURBRL']['bid'].replace('.', ',')}")
+        st.markdown("")
+        st.markdown("**:material/currency_pound: Cotação do Libra**")
+        st.markdown(f"Moeda $   : {get_cota_gb['GBPBRL']['name']}")
+        st.markdown(f"Data/Hora : {format_date_br(get_cota_gb['GBPBRL']['create_date'])}")
+        st.markdown(f"Valor (R$): {get_cota_gb['GBPBRL']['bid'].replace('.', ',')}")
 
 with col2.expander("Markdown Incrível", icon="💯"):
     st.markdown("*Streamlit* é **realmente** ***legal***.")
@@ -48,3 +48,29 @@ with col2.expander("Markdown Incrível", icon="💯"):
     """)
     st.markdown("Here's a bouquet &mdash;"
                 " :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+
+with col3.expander("Valor por extenso", icon="💯"):
+    def num_extenso(n: int) -> str:
+        unidade = ["zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove"]
+        dez_x = ["dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"]
+        dezena = ["", "", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa", "cem"]
+
+        return dezena[n // 10] + " e " + unidade[n % 10] \
+            if n >= 20 and n % 10 != 0 else dezena[n // 10] \
+            if n >= 20 and n % 10 == 0 else dez_x[n % 10] \
+            if 10 <= n < 20 and n % 10 == 0 else dez_x[n - 10] \
+            if 10 <= n < 20 and n % 10 != 0 else unidade[n] \
+            if n < 10 and n % 10 != 0 else unidade[0]
+
+
+    st.number_input("Digite um número entre 0 e 100:", min_value=0, max_value=100, value=0, key="num_extenso")
+
+    st.markdown("")
+
+    st.columns([1, 2, 1])[1].button("**Converter**", key="extenso", type="primary", icon=":material/pin:",
+                                    use_container_width=True)
+
+    st.markdown("")
+
+    if st.session_state["extenso"]:
+        st.markdown(f"O número por extenso é {num_extenso(st.session_state['num_extenso'])}.")
--- pages/1_config.py
+++ pages/1_config.py
@@ -1,9 +1,8 @@
 # esse arquivo vem juntamente com o arquivo 'app.py' que é página principal
-
 import streamlit as st
 from app import toggle_sidebar
 
 st.header(":material/settings: Configuração")
 
-if st.button("**Voltar**", icon=":material/reply:", on_click=toggle_sidebar):
+if st.button("**Voltar**", type="primary", icon=":material/reply:", on_click=toggle_sidebar):
     st.switch_page("app.py")
