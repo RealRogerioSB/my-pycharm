3fcbd2197b6981a26e7e576d393fe10ef9839ea2
Rogério Balloussier <rogerioballoussier@icloud.com>
2025-05-23 19:03:34 -0300

    updated commit...

--- unibb.py
+++ unibb.py
@@ -1,84 +1,38 @@
-import time
-
 import pandas as pd
 import streamlit as st
-from sqlalchemy import text
-from streamlit.connections import SQLConnection
-
-engine: SQLConnection = st.connection(name="AIVEN-PG", type=SQLConnection)
 
 
 @st.cache_data(show_spinner="⏳Obtendo os dados, aguarde...")
 def load_course() -> pd.DataFrame:
-    return engine.query(sql="SELECT id_curso, nm_curso, hr_curso FROM unibb", show_spinner=False, ttl=0)
+    return pd.read_csv("~/Documents/unibb.csv", parse_dates=["dt_curso"])
 
 
-@st.dialog(title="Adicionar Curso")
-def add_course():
-    col1, col2 = st.columns(2)
-    col1.number_input("Código do Curso", min_value=1, max_value=999999, key="new_id_curso")
-    col2.number_input("Carga Horária", min_value=1, max_value=100, key="new_hr_curso")
-    st.text_input("Nome do Curso", placeholder="Nome do Curso", key="new_nm_curso")
-    st.button("**Incluir**", key="btn_add", type="primary", icon=":material/add_circle:")
+def save_csv(frame: pd.DataFrame) -> None:
+    if frame.empty:
+        st.toast("**A planilha não foi alterada...**", icon=":material/error:")
 
-    if st.session_state["btn_add"]:
-        with engine.session as session:
-            session.execute(
-                statement=text("INSERT INTO unibb (id_curso, nm_curso, hr_curso) VALUES (:id, :nm, :hr)"),
-                params=dict(
-                    id=st.session_state["new_id_curso"],
-                    nm=st.session_state["new_nm_curso"],
-                    hr=st.session_state["new_hr_curso"]
-                )
-            )
-            session.commit()
-            st.toast("**Curso adicionado com sucesso!**", icon=":material/check_circle:")
-            st.session_state["course"] = load_course().copy()
-            time.sleep(1)
-        st.rerun()
+    else:
+        frame.to_csv("~/Documents/unibb.csv", index=False)
+        st.cache_data.clear()
+        st.toast("**Planilha alterada com sucesso!**", icon=":material/check_circle:")
 
-
-@st.cache_data(show_spinner="⏳Obtendo os dados, aguarde...")
-def load_duplicity() -> pd.DataFrame:
-    return engine.query(
-        sql="""SELECT id_curso, nm_curso, hr_curso
-               FROM unibb
-               WHERE nm_curso IN (SELECT nm_curso FROM unibb GROUP BY nm_curso HAVING COUNT(nm_curso) > 1)
-               ORDER BY nm_curso, id_curso""",
-        show_spinner=False,
-        ttl=0,
-    )
-
-
-if "course" not in st.session_state:
-    st.session_state["course"] = load_course()
 
 tab1, tab2 = st.tabs(["**Cursos da UniBB**", "**Cursos Duplicados**"])
 
 with tab1:
-    st.data_editor(
-        data=st.session_state["course"],
+    editor = st.data_editor(
+        data=load_course(),
         hide_index=True,
         column_config={
             "id_curso": st.column_config.NumberColumn(label="Código", width="small"),
             "nm_curso": st.column_config.TextColumn(label="Curso", width="large"),
-            "hr_curso": st.column_config.NumberColumn(label="Horas", width="small"),
+            "dt_curso": st.column_config.DateColumn(label="Data", format="DD/MM/YYYY"),
+            "cg_curso": st.column_config.NumberColumn(label="Horas", width="small"),
         },
-        key="de_course",
+        key="editor",
+        num_rows="dynamic",
         row_height=25,
     )
 
-    st.button("**Adicionar**", key="add_course", type="primary", icon=":material/add_circle:", on_click=add_course)
-
-with tab2:
-    st.data_editor(
-        data=load_duplicity(),
-        hide_index=True,
-        column_config={
-            "id_curso": st.column_config.NumberColumn(label="Código", width="small"),
-            "nm_curso": st.column_config.TextColumn(label="Curso", width="large"),
-            "hr_curso": st.column_config.NumberColumn(label="Horas", width="small"),
-        },
-        key="de_duplicity",
-        row_height=25,
-    )
+    st.button("**Adicionar**", key="add_course", type="primary",
+              icon=":material/add_circle:", on_click=save_csv, args=(editor,))
