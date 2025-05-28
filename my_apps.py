729d31d34fa6bed012a0ee0655f1443ce626e3fb
Rogério Balloussier <rogerioballoussier@icloud.com>
2025-05-27 21:22:17 -0300

    updated commit...

--- my_apps.py
+++ my_apps.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import pandas as pd
 import requests
 import streamlit as st
+from unidecode import unidecode
 
 st.set_page_config(
     page_title="Streamlit Apps",
@@ -40,7 +41,7 @@ with col1.expander("Cotação de Moedas Estrangeiras", icon="💰"):
             use_container_width=True,
             column_config={
                 "name": st.column_config.TextColumn("Moeda $"),
-                "create_date": st.column_config.DateColumn("Data/Hora", format="DD/MM/YYYY"),
+                "create_date": st.column_config.DatetimeColumn("Data/Hora", format="DD/MM/YYYY HH:MM:SS"),
                 "bid": st.column_config.NumberColumn("Valor R$", format="dollar"),
             },
         )
@@ -68,14 +69,69 @@ with col3.expander("Valor por extenso", icon="💯"):
             if n < 10 and n % 10 != 0 else unidade[0]
 
 
-    st.number_input("Digite um número entre 0 e 100:", min_value=0, max_value=100, value=0, key="num_extenso")
+    st.number_input("Digite um número entre 0 e 100:", value=0, key="extenso", min_value=0, max_value=100)
 
-    st.markdown("")
+    msg_extenso = st.empty()
 
-    st.columns([1, 2, 1])[1].button("**Converter**", key="extenso", type="primary", icon=":material/pin:",
-                                    use_container_width=True)
+col1, col2, col3 = st.columns(3, border=True)
 
-    st.markdown("")
+with col1.expander("Código do Morse", icon="💰"):
+    def code_morse(txt_morse: str) -> str:
+        code: dict[str, str] = {
+            "A": ".-", "B": "-...", "C": "-.-.", "D": "-..", "E": ".", "F": "..-.", "G": "--.",
+            "H": "....", "I": "..", "J": ".---", "K": "-.-", "L": ".-..", "M": "--", "N": "-.",
+            "O": "---", "P": ".--.", "Q": "--.-", "R": ".-.", "S": "...", "T": "-", "U": "..-",
+            "V": "...-", "W": ".--", "X": "-..-", "Y": "-.--", "Z": "--..", "1": ".----",
+            "2": "..---", "3": "...--", "4": "....-", "5": ".....", "6": "-....", "7": "--...",
+            "8": "---..", "9": "----.", "0": "-----", ",": "--..--", ".": ".-.-.-", "?": "..--..",
+            "/": "-..-.", "-": "-....-", "(": "-.--.", ")": "-.--.-", "!": "-.-.--", " ": " ",
+            "'": ".----.", ":": "---..."
+        }
 
-    if st.session_state["extenso"]:
-        st.markdown(f"O número por extenso é {num_extenso(st.session_state['num_extenso'])}.")
+        return " ".join(code[morse] for morse in unidecode(txt_morse).upper())
+
+
+    st.text_input("Digite o texto a ser convertido para Código Morse:", key="txt_morse")
+
+    msg_morse = st.empty()
+
+with col2.expander("Checador de IP", icon="💯"):
+    # faz uma requisição GET ao site Google
+    st.text_input("Digite o site para verificar o status:", key="site", icon=":material/public:")
+
+    msg_site = st.empty()
+
+with col3.expander("Algaritmo Romano", icon="💯"):
+    def alg_romano(num_rom):
+        val_int = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        val_rom = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        num_romano = ""
+        b = 0
+        while num_rom > 0:
+            for _ in range(num_rom // val_int[b]):
+                num_romano += val_rom[b]
+                num_rom -= val_int[b]
+            b += 1
+        return num_romano
+
+
+    st.number_input("Digite um número a ser convertido:", key="romano", min_value=0, max_value=9999)
+
+    msg_romano = st.empty()
+
+if st.session_state["extenso"]:
+    msg_extenso.markdown(f"O número por extenso é {num_extenso(st.session_state['extenso'])}.")
+
+if st.session_state["txt_morse"]:
+    msg_morse.markdown(code_morse(st.session_state['txt_morse']))
+
+if st.session_state["site"]:
+    try:
+        status: int = requests.get(f"https://{st.session_state['site']}").status_code
+        msg_site.markdown(f"O status do site do IP é {status}.")
+
+    except requests.exceptions.ConnectionError:
+        st.markdown("O site não está acessível.")
+
+if st.session_state["romano"]:
+    msg_romano.markdown(f"O número convertido por romano é {alg_romano(st.session_state['romano'])}.")
