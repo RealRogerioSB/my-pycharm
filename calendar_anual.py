4d50d56bbfab8e40fcc6e3ec32e998c2bad1daef
Rogério Balloussier <rogerioballoussier@icloud.com>
2025-06-02 16:42:00 -0300

    deleted commit...

--- calendar_anual.py
+++
@@ -1,6 +0,0 @@
-import calendar
-import locale
-from datetime import datetime
-
-locale.setlocale(category=locale.LC_ALL, locale="pt_BR.UTF-8")
-print(calendar.calendar(theyear=datetime.now().year))
